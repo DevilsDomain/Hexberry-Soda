6797d18b9829e095159c32e5cb1c0a5ca3fb2471
DevilsDomain <alaraerzincan@gmail.com>
2024-04-21 12:55:55 +0200

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { Scroll, ScrollControls } from "@react-three/drei"
 import { SheetProvider, editable as e } from "@theatre/r3f"
 import Scene from "./components/Scene";
 import './index.css'
+import Navbar from "./components/Navbar";
 
 function App({sheet}) {
   return (
     <>
-    <ScrollControls pages={5} damping={1} maxSpeed={0.1}>
+    <ScrollControls pages={5} damping={0.3} maxSpeed={0.7}>
       <SheetProvider sheet={sheet} >
         <Scene />
       </SheetProvider>
       <Scroll html>
+        <Navbar />
         <div className="w-screen h-screen">
           <h1 className="text-3xl font-bold underline">Landing page</h1>
         </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../assets/Logo.svg';
+import '../index.css';
+
+function Navbar() {
+  return (
+    <nav className="fixed top-0 w-full flex items-center justify-center p-4 gloria-hallelujah-regular">
+      <div className="flex items-center gap-36">
+        <img src={logo} alt='Logo' />
+        <h3 className="cursor-pointer hover:underline">HOME</h3> 
+        <h3 className="cursor-pointer hover:underline">ABOUT</h3> 
+        <h3 className="cursor-pointer hover:underline">FLAVORS</h3> 
+        <h3 className="cursor-pointer hover:underline">STORE</h3> 
+        <h3 className="cursor-pointer hover:underline">CONTACT</h3> 
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
