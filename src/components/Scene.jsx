0cce51dcea25f3729c9a2d58fc6969772da751dc
DevilsDomain <alaraerzincan@gmail.com>
2024-04-21 21:50:02 +0200

    added Text

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,4 +1,4 @@
-import { Environment, useScroll } from '@react-three/drei';
+import { Environment, useScroll, Text, OrbitControls, Center, GradientTexture, GradientType  } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { val } from '@theatre/core';
 import { useCurrentSheet, editable as e } from '@theatre/r3f'
@@ -20,6 +20,19 @@ function Scene() {
     <color attach="background" args={["#FCF8F5"]} />
     <Environment preset='warehouse' />
     <ambientLight intensity={6} />
+    <Center position={[0.5,1.9,-1]}>
+        <Text font={".//fonts/AlloyInk-nRLyO.ttf"} fontSize={2.3}>
+            HEXABERRY
+            <meshBasicMaterial>
+            <GradientTexture
+            stops={[0, 1]} // As many stops as you want
+            colors={['#FD55A6', '#FCC737']} // Colors need to match the number of stops
+            size={1024} // Size is optional, default = 1024
+            type={GradientType.Radial}
+            />
+            </meshBasicMaterial>
+        </Text>
+    </Center>
     <HexaBerryCan5 scale={0.033} position={[0,-0.3,0]} />
     </>
   )
