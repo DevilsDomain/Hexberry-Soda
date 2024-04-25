bd116e2b278f39833ad8ef15b0dc6f7ee0b5e3ae
DevilsDomain <alaraerzincan@gmail.com>
2024-04-25 12:50:32 +0200

    added assets, updated fonts

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App({sheet}) {
       </SheetProvider>
       <Scroll html className="gloria-hallelujah-regular">
         <Navbar />
+        {/* Landing Page */}
         <div className="w-screen h-screen relative flex flex-col items-center mt-20">
           <img src={lines} alt="excitment lines" />
           <h6 className="text-sm">Where Flavor Meets Fun!</h6>
@@ -28,15 +29,19 @@ function App({sheet}) {
           <img src={arrowLoop} alt="Loop arrow" className="absolute left-100 bottom-80" />
           <img src={arrowUp} alt="up arrow" className="absolute right-100 bottom-72" />
         </div>
+        {/* Flavor 1 */}
         <div className="w-screen h-screen"> 
           <h1 className="text-3xl font-bold underline">Lemon Lime</h1>
         </div>
+        {/* Flavor 2 */}
         <div className="w-screen h-screen">
           <h1 className="text-3xl font-bold underline">Berries</h1>
         </div>
+        {/* Flavor 3 */}
         <div className="w-screen h-screen">
           <h1 className="text-3xl font-bold underline">Exotic</h1>
         </div>
+        {/* Podium */}
         <div className="w-screen h-screen">
           <h1 className="text-3xl font-bold underline">Podium</h1>
         </div>
