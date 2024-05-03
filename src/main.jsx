9c854e13b9b59675e8f64559919b062543b827bb
DevilsDomain <alaraerzincan@gmail.com>
2024-05-03 15:35:55 +0200

    added loader

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,13 @@ import * as THREE from "three"
 import './index.css'
 import studio from '@theatre/studio'
 import extension from '@theatre/r3f/dist/extension'
-import { getProject, types } from "@theatre/core"
+import { getProject } from "@theatre/core"
+import hexaberryAnimationState from './HexBerry Animation.theatre-project-state.json'
+import { Loader } from '@react-three/drei'
 
-studio.initialize();
-studio.extend(extension);
-const sheet = getProject('HexBerry Animation').sheet('Scene');
+// studio.initialize();
+// studio.extend(extension);
+const sheet = getProject('HexBerry Animation', { state: hexaberryAnimationState }).sheet('Scene');
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -30,6 +32,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App sheet={sheet} />
     </Suspense>
     </Canvas>
+    <Loader />
   </React.StrictMode>,
 )
 
